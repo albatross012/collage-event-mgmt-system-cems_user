ade7f542242711edb10ad011e915317780495e18
Midhun Mathew <101048514+albatross012@users.noreply.github.com>
2022-07-19 18:01:57 +0530

    loading

--- cems/lib/register_event.dart
+++ cems/lib/register_event.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:developer';
 import 'package:cems/main.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -16,6 +17,7 @@ class RegisterEvent extends StatefulWidget {
 
 class _RegisterEventState extends State<RegisterEvent> {
   List<Event> events = [];
+  bool isLoading = true;
   final storage = const FlutterSecureStorage();
   Future<bool> getEvents() async {
     try {
@@ -45,112 +47,123 @@ class _RegisterEventState extends State<RegisterEvent> {
 
   @override
   void initState() {
-    getEvents();
+    getEvents().then((value) {
+      setState(() {
+        isLoading = false;
+      });
+    });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height,
-                child: Material(
-                  child: ListView.builder(
-                    padding: EdgeInsets.only(bottom: 200),
-                    itemBuilder: (context, index) => Container(
-                      margin: EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset(1, -1),
-                            color: Colors.black.withAlpha(40),
-                          )
-                        ],
-                        borderRadius: BorderRadiusDirectional.circular(
-                          28,
+      body: isLoading
+          ? const Center(
+              child: CupertinoActivityIndicator(
+                color: Color(0xff36CDC6),
+                radius: 40,
+              ),
+            )
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height,
+                      child: Material(
+                        child: ListView.builder(
+                          padding: EdgeInsets.only(bottom: 200),
+                          itemBuilder: (context, index) => Container(
+                            margin: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  offset: Offset(1, -1),
+                                  color: Colors.black.withAlpha(40),
+                                )
+                              ],
+                              borderRadius: BorderRadiusDirectional.circular(
+                                28,
+                              ),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ImageSlideshow(
+                                  width: double.infinity,
+                                  height: 300,
+                                  initialPage: 0,
+                                  indicatorColor: Colors.blue,
+                                  indicatorBackgroundColor: Colors.grey,
+                                  children: events[index]
+                                      .imageUrl
+                                      .map((e) => Container(
+                                            height: 300,
+                                            width: double.infinity,
+                                            decoration: BoxDecoration(
+                                              image: DecorationImage(
+                                                fit: BoxFit.fill,
+                                                image: NetworkImage(e),
+                                              ),
+                                            ),
+                                          ))
+                                      .toList(),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    events[index].eventName.toString(),
+                                    style: GoogleFonts.roboto(
+                                      fontSize: 23,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    events[index].description.toString(),
+                                    style: GoogleFonts.roboto(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ),
+                                Center(
+                                  child: MaterialButton(
+                                    onPressed: () {},
+                                    minWidth: 152,
+                                    height: 61,
+                                    color: const Color(0xff36CDC6),
+                                    elevation: 0,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(19),
+                                    ),
+                                    child: const Text(
+                                      "REGISTER",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 32,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          itemCount: events.length,
                         ),
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ImageSlideshow(
-                            width: double.infinity,
-                            height: 300,
-                            initialPage: 0,
-                            indicatorColor: Colors.blue,
-                            indicatorBackgroundColor: Colors.grey,
-                            children: events[index]
-                                .imageUrl
-                                .map((e) => Container(
-                                      height: 300,
-                                      width: double.infinity,
-                                      decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                          fit: BoxFit.fill,
-                                          image: NetworkImage(e),
-                                        ),
-                                      ),
-                                    ))
-                                .toList(),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              events[index].eventName.toString(),
-                              style: GoogleFonts.roboto(
-                                fontSize: 23,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              events[index].description.toString(),
-                              style: GoogleFonts.roboto(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ),
-                          Center(
-                            child: MaterialButton(
-                              onPressed: () {},
-                              minWidth: 152,
-                              height: 61,
-                              color: const Color(0xff36CDC6),
-                              elevation: 0,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(19),
-                              ),
-                              child: const Text(
-                                "REGISTER",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 32,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
                     ),
-                    itemCount: events.length,
-                  ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
